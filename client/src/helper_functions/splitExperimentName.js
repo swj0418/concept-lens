d32b03ff068beb5e8e2a4500b9f01c83b0b89fa1
sangwon <swj0418@hotmail.com>
2025-03-08 17:58:28 -0600

    image paths rerouted

--- client/src/helper_functions/splitExperimentName.js
+++ client/src/helper_functions/splitExperimentName.js
@@ -1,13 +1,16 @@
 export function splitExperimentName(expName) {
-    const splits = expName.split("-")
-    const domainName = splits[0]
-    const methodName = splits[1]
+   const splits = expName.split("-")
+   const domainName = splits[0]
+   const methodName = splits[1]
 
-    const layerName = splits[2]
-    // const layerSubName = splits[3].split("_")[1]
-    const layerSubName = null
 
-    const applicationName = splits[3]
+   const layerName = splits[2]
+   // const layerSubName = splits[3].split("_")[1]
+   const layerSubName = null
 
-    return [domainName, methodName, applicationName, layerName, layerSubName]
+
+   const applicationName = splits[3]
+
+
+   return [domainName, methodName, applicationName, layerName, layerSubName]
 }
--- server/conceptlens/views_metadata.py
+++ server/conceptlens/views_metadata.py
@@ -1,71 +1,89 @@
 import os
 import json
 
+
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 import natsort
 from django.conf import settings
 
+
 SERVED_DATA_ROOT = settings.SERVED_DATA_ROOT
+
 
 @csrf_exempt
 def get_setting(request):
-    """
+   """
 
-    Returns:
 
-    """
-    data = json.loads(request.body)
-    experiment_name = data['experiment_name']
+   Returns:
 
-    # Read Data
-    root = os.path.join('served_data', experiment_name)
 
-    # Read config
-    with open(os.path.join(root, 'setting.json'), 'r') as file:
-        setting = json.load(file)
+   """
+   data = json.loads(request.body)
+   experiment_name = data['experiment_name']
 
-    response = json.dumps(setting, indent=2)
-    return JsonResponse(response, safe=False)
+
+   # Read Data
+   root = os.path.join('served_data', experiment_name)
+
+
+   # Read config
+   with open(os.path.join(root, 'setting.json'), 'r') as file:
+       setting = json.load(file)
+
+
+   response = json.dumps(setting, indent=2)
+   return JsonResponse(response, safe=False)
+
+
 
 
 @csrf_exempt
 def available_experiments(request):
-    # List the experiments in the served data directory
-    found_experiments = natsort.natsorted(os.listdir(SERVED_DATA_ROOT))
-    # Remove unwanted files (like .DS_Store on macOS)
-    if '.DS_Store' in found_experiments:
-        found_experiments.remove('.DS_Store')
+   # List the experiments in the served data directory
+   found_experiments = natsort.natsorted(os.listdir(SERVED_DATA_ROOT))
+   # Remove unwanted files (like .DS_Store on macOS)
+   if '.DS_Store' in found_experiments:
+       found_experiments.remove('.DS_Store')
 
-    # Create a list of experiment dictionaries following the original EXPERIMENT structure
-    experiments = [{"id": exp, "name": exp} for exp in found_experiments]
 
-    # Return the experiments under the key "EXPERIMENT"
-    return JsonResponse({"EXPERIMENT": experiments})
+   # Create a list of experiment dictionaries following the original EXPERIMENT structure
+   experiments = [{"id": exp, "name": exp} for exp in found_experiments]
+
+
+   # Return the experiments under the key "EXPERIMENT"
+   return JsonResponse({"EXPERIMENT": experiments})
+
+
 
 
 @csrf_exempt
 def request_setting(request):
-    data = json.loads(request.body)
-    experiment_name = data['experiment_name']
+   data = json.loads(request.body)
+   experiment_name = data['experiment_name']
 
-    # Read Data
-    root = os.path.join('served_data', experiment_name)
 
-    with open(os.path.join(root, 'setting.json')) as file:
-        config = json.load(file)
+   # Read Data
+   root = os.path.join('served_data', experiment_name)
 
-    experiment_name = config['experiment_name']
-    n_code = config['n_code']
-    n_method = config['n_method']
-    n_direction_per_method = config['n_direction']
 
-    return JsonResponse({
-        'data': {
-            'experiment_name': experiment_name,
-            'feature_space': config['feature_space'],
-            'n_code': n_code,
-            'n_method': n_method,
-            'n_direction_per_method': n_direction_per_method
-        }
-    })
+   with open(os.path.join(root, 'setting.json')) as file:
+       config = json.load(file)
+
+
+   experiment_name = config['experiment_name']
+   n_code = config['n_code']
+   n_method = config['n_method']
+   n_direction_per_method = config['n_direction']
+
+
+   return JsonResponse({
+       'data': {
+           'experiment_name': experiment_name,
+           'feature_space': config['feature_space'],
+           'n_code': n_code,
+           'n_method': n_method,
+           'n_direction_per_method': n_direction_per_method
+       }
+   })
